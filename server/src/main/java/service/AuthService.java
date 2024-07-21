7b5e31a5c0ddfa2680dd54089950ea54e3b818cf
cami <camillekrieger@yahoo.com>
2024-07-20 20:21:06 -0600

    clear implemented

--- server/src/main/java/service/AuthService.java
+++ server/src/main/java/service/AuthService.java
@@ -1,4 +1,18 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+
 public class AuthService {
+
+    public void clear() throws DataAccessException {
+        MemoryAuthDAO mad = new MemoryAuthDAO();
+        MemoryGameDAO mgd = new MemoryGameDAO();
+        MemoryUserDAO mud = new MemoryUserDAO();
+        mad.clear();
+        mgd.clear();
+        mud.clear();
+    }
 }
