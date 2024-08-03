086334cad90ea9f47fcae097e98c1dc28cdc1af5
cami <camillekrieger@yahoo.com>
2024-08-02 23:29:43 -0600

    serverfacade put together

--- client/src/main/java/serverfacade/ServerFacade.java
+++ client/src/main/java/serverfacade/ServerFacade.java
@@ -1,6 +1,7 @@
 package serverfacade;
 
 import chess.ChessGame;
+import com.google.gson.Gson;
 import model.AuthData;
 import model.GameData;
 import model.UserData;
@@ -9,6 +10,9 @@ import ui.JoinGameRequest;
 import ui.LoginRequest;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.*;
 
 public class ServerFacade {
@@ -61,19 +65,52 @@ public class ServerFacade {
         return makeRequest("PUT", path, joinGameRequest, null);
     }
 
-    private <T> T makeRequest(String method, String path, Object request, Object response) throws URISyntaxException, IOException {
-        URL url = (new URI(serverURL + path)).toURL();
-        HttpURLConnection http = (HttpURLConnection) url.openConnection();
-        http.setRequestMethod(method);
-        http.setDoOutput(true);
-        writeBody();
-        http.connect();
-        return readBody();
+    private <T> T makeRequest(String method, String path, Object request, Class<T> response) throws URISyntaxException, IOException {
+        try {
+            URL url = (new URI(serverURL + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, response);
+        } catch (Exception e){
+            throw new IOException();
+        }
     }
 
-    private static void writeBody(){}
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            throw new IOException();
+        }
+    }
 
-    private static <T> T readBody(){
-        return null;
+    private boolean isSuccessful(int status){
+        return status / 100 == 2;
+    }
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null){
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
     }
 }
