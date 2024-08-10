d61dabe280ed6db3c087267baea611685acb5578
cami <camillekrieger@yahoo.com>
2024-08-10 14:14:10 -0600

    websocket structure put together

---
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -0,0 +1,26 @@
+package server.websocket;
+
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.commands.UserGameCommand;
+
+@WebSocket
+public class WebSocketHandler {
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message){
+        UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
+        switch (command.getCommandType()) {
+            case CONNECT -> connect(command.getGameID(), session, message);
+            case MAKE_MOVE -> makeMove(command);
+            case LEAVE -> leave();
+            case RESIGN -> resign();
+        }
+    }
+
+    private void connect(int gameID, Session session, String message){
+        //do something
+    }
+}
---
+++ server/src/main/java/server/websocket/WebSocketSessions.java
@@ -0,0 +1,31 @@
+package server.websocket;
+
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class WebSocketSessions {
+    private final Map<Integer, Set<Session>> sessions = new HashMap<>();
+
+    public void addSessionToGame(int gameID, Session session){
+        sessions.computeIfAbsent(gameID, k -> new HashSet<>());
+        sessions.get(gameID).add(session);
+    }
+
+    public void removeSessionFromGame(int gameID, Session session){
+        sessions.get(gameID).remove(session);
+    }
+
+    public void removeSession(Session session){
+        for(int gameID : sessions.keySet()){
+            sessions.get(gameID).remove(session);
+        }
+    }
+
+    public Set<Session> getSessionsFromGame(int gameID){
+        return sessions.get(gameID);
+    }
+}
