7e9c9e68744cbb0a0dff8663060118e6422a5319
cami <camillekrieger@yahoo.com>
2024-08-10 17:19:06 -0600

    updated web socket handler

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -5,6 +5,7 @@ import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.commands.MakeMoveCommand;
 import websocket.commands.UserGameCommand;
 
 import javax.management.Notification;
@@ -16,9 +17,9 @@ public class WebSocketHandler {
 
     @OnWebSocketMessage
     public void onMessage(Session session, String message){
-        UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
+        MakeMoveCommand command = new Gson().fromJson(message, MakeMoveCommand.class);
         switch (command.getCommandType()) {
-            case CONNECT -> connect(command.getGameID(), session, message);
+            case CONNECT -> connect(command.getGameID(), session);
             case MAKE_MOVE -> makeMove(command.getGameID(), command.getAuthToken(), session, message, command.getMove());
             case LEAVE -> leave(command.getGameID(), session, message);
             case RESIGN -> resign(command.getGameID(), message);
@@ -28,7 +29,6 @@ public class WebSocketHandler {
     private void connect(int gameID, Session session){
         //join or observe a game
         sessionsSet.addSessionToGame(gameID, session);
-        String response = String.format("%s joined %s as %s", username, gameName, color);
     }
 
     private void makeMove(int gameID, String authToken, Session session, String message, ChessMove move){
@@ -37,6 +37,7 @@ public class WebSocketHandler {
 
     private void leave(int gameID, Session session, String message){
         //leave the game
+        sessionsSet.removeSessionFromGame(gameID, session);
     }
 
     private void resign(int gameID, String message){
